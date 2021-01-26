30a33854f20077fabe450b2bff2141d4186d970a
Brian DiRito <bdirito91@gmail.com>
2021-01-25 20:58:06 -0500

    more logiin work

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const getStringToken = () => {
-    fetch("/login")
-    .then(res => res.json())
-    .then(data => console.log(data))
-  }
-
-  getStringToken()
-  
   return (
     <div className="App">
       <header className="App-header">
