a54c472749101cac54ab848d46957e13703e2010
Brian DiRito <bdirito91@gmail.com>
2021-01-25 19:50:45 -0500

    connected env file

--- server/app.js
+++ server/app.js
@@ -2,9 +2,22 @@ const express = require("express")
 const axios = require("axios")
 
 const port = process.env.PORT || 8000
-
 const app = express()
+
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
+const callbackUri = process.env.REACT_APP_SPOTIFY_CALLBACK_URL
+
 
 app.listen(port, () => {
     console.log(`LISTENING ON PORT ${port}`)
+})
+
+app.get("/login", (req, res) => {
+    res.send({
+        "message": "pinged login route"
+    })
 })
