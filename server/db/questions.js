c09ba915b1fb4e87081ed24af397ab7b0beb8c80
Diana Yoo <diana@thesistesting.com>
2021-02-25 18:30:37 -0500

    update db and add images

--- server/db/questions.js
+++ server/db/questions.js
@@ -1,6 +1,8 @@
 const questions = [
     {
+        id: 0,
         question: "Which of these colors are you vibing with?",
+        images: [],
         answers: {
             "Blue": "sadGirlHour",
             "Yellow": "sunshineAndLollipops",
@@ -12,14 +14,25 @@ const questions = [
         }
     },
     {
+        id: 1,
         question: "What image do you see?",
+        images: ["inkblot.jpg"],
         answers: {
+            "Gliding Lizard": "apocalypseMeow",
+            "Otter": "sunshineAndLollipops",
+            "Coat of Arms": "girlPower",
+            "X-ray": "sadGirlHour",
+            "Butterfly": "oldiesAndGoodies",
+            "Tree": "boogieShoes",
+            "Panther on a Rock": "what"
         }
     },
     {
+        id: 2,
         question: "What do you want to do right now?",
+        images: [],
         answers: {
-            "Crush the Patriarchy": "Girl Power",
+            "Crush the Patriarchy": "girlPower",
             "Frolic in the Garden": "sunshineAndLollipops",
             "Feel the Rain": "sadGirlHour",
             "Become a Muppet": "what",
@@ -28,19 +41,31 @@ const questions = [
         }
     },
     {
+        id: 3,
         question: "White type of cat are you?",
+        images: [
+            "sandCat.png",
+            "bombayCat.png",
+            "pussInBoots.png",
+            "grumpyCat.png",
+            "nala.png",
+            "dancingCat.png",
+            "garfield.png"
+        ],
         answers: {
             "Sand Cat": "sunshineAndLollipops",
             "Bombay Cat": "sadGirlHour",
             "Puss in Boots": "what",
             "Grumpy Cat": "apocalypseNow",
             "Nala": "girlPower",
-            "ImageUrl": "boogieShoes",
+            "Dancing Cat": "boogieShoes",
             "Garfield": "oldiesAndGoodies"
         }
     },
     {
+        id: 4,
         question: "Quick you've become a flavor - pick one!",
+        images: [],
         answers: {
             "Sweet": "sunshineAndLollipops",
             "Sour": "boogieShoes",
