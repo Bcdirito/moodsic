cf8b712c008864955178a07c4a3382e87ea3b025
Diana Yoo <diana@thesistesting.com>
2021-03-31 20:10:28 -0400

    set up web player sdk thingy

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react';
 import Quiz from './components/quiz/Quiz.js';
-import Playlist from './components/playlist/Playlist.js';
 import './App.scss';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [accessToken, setAccessToken] = useState(null);
-  const [player, setPlayer] = useState(null);
 
   useEffect(() => {
+    // return function disconnectPlayer() {
+    //   console.log('Disconnecting player...');
+    //   player.disconnect();
+    // };
     if (window.location.pathname.includes('access_token')) {
       let token;
       let path = window.location.pathname.split('/');
@@ -26,43 +28,59 @@ function App() {
   }, []);
 
   useEffect(() => {
-    window.onSpotifyWebPlaybackSDKReady = () => {
-      if (accessToken) {
-        console.log('Spotify initialized');
-        setLoggedIn(true);
+    if (window.Spotify) {
+      setLoggedIn(true);
+    } else {
+      window.onSpotifyWebPlaybackSDKReady = () => {
+        if (accessToken) {
+          console.log('Spotify initialized');
 
-        // Initialize Spotify player
-        const player = new window.Spotify.Player({
-          name: "Brian's Player :)",
-          getOAuthToken: callback => {
-            // Run code to get a fresh access token
+          // Initialize Spotify player
+          const player = new window.Spotify.Player({
+            name: "Brian's Player :)",
+            getOAuthToken: callback => {
+              // Run code to get a fresh access token
 
-            callback(accessToken);
-          },
-          volume: 0.5
-        });
+              callback(accessToken);
+            },
+            volume: 0.5
+          });
 
-        player.connect().then(success => {
-          if (success) {
-            console.log(
-              'The Web Playback SDK successfully connected to Spotify!'
-            );
-            setPlayer(player);
-          }
-        });
-      }
-    };
-
-    // return function disconnectPlayer() {
-    //   console.log('Disconnecting player...');
-    //   player.disconnect();
-    // };
+          player.connect().then(success => {
+            if (success) {
+              console.log(
+                'The Web Playback SDK successfully connected to Spotify!'
+              );
+              setLoggedIn(true);
+            }
+          });
+        }
+      };
+    }
   }, [accessToken]);
+
+  useEffect(() => {
+    if (loggedIn) {
+      if (window.location.pathname.includes('access_token')) {
+        let token;
+        let path = window.location.pathname.split('/');
+        path = path[1].split('&');
+
+        // Get access token value from path name
+        for (const key of path) {
+          if (key.includes('access_token')) {
+            token = key.split('=')[1];
+          }
+        }
+
+        setAccessToken(token);
+      }
+    }
+  }, [loggedIn]);
 
   return (
     <div className='App'>
-      {/* <Quiz /> */}
-      <Playlist loggedIn={loggedIn} />
+      <Quiz loggedIn={loggedIn} />
     </div>
   );
 }
